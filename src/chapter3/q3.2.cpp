2a1d2f27b49081cfeb5d2b9dcdfcbcc360f5655b
eginoy <eginoy.oheso@gmail.com>
2021-02-20 14:34:15 +0900

    add q3.2

---
+++ src/chapter3/q3.2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+//章末問題3.1
+//与えられたN個の整数からa[i] = vをいくつ含むか求める。
+
+int main()
+{
+    //入力を受け取る
+    int N, v;
+    cin >> N >> v;
+    vector<int> a(N);
+    for (int i = 0; i < N; ++i) cin >> a[i];
+
+    int found_count = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        if (a[i] == v) ++found_count;
+    }
+
+    //結果出力
+    cout << found_count << endl;
+}
